87806ce1c7ca7a4d550009a96c90a4e6b854a275
NIKHIL VERMA <nikhilverma2004n@gmail.com>
2025-06-14 14:25:14 +0530

    issue of first time wrong loading resloved

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,12 +1,18 @@
-// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://instantchat-1-s5vq.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
-    Authorization: localStorage.getItem("token"),
   },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
 });
 
 export default api;
